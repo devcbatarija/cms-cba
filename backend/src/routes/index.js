52ca1601378d5e6cf6ea05f8a83100550adcf6ed
ronaldgallardoarce <ronaldgallardoarce78@gmail.com>
2023-08-18 11:38:37 -0400

    contollers

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,9 @@
 const {Router} =require("express");
 const testRouter=require("./testRouter");
+const publicacionRoutes=require('./publicacionRoutes');
 const router=Router();
 
 router.use('/test',testRouter) //"http:localhost:3001/api/test"
-
+router.use('/publication',publicacionRoutes);
 module.exports=router;
 
