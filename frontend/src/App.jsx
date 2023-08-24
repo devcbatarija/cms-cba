82d80a27c9c873aee0bc8892a178bd1650d06261
olivio-git <subelzaolivitocabezas@gmail.com>
2023-08-24 08:34:56 -0400

    install M-UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,22 @@
 import './App.css'
-import { Route, Routes, useParams } from 'react-router-dom';
+import { Route, Routes, useNavigate, useParams } from 'react-router-dom';
 import Home from './components/home/home';
 import NavBar from './components/navBar/navBar';
 import About from './components/about/about';
-import { useState } from 'react';
+import Login from './components/auth/auth.login';
+import { useEffect } from 'react';
+import Register from './components/auth/auth.register';
+import { useSelector } from 'react-redux';
 function App() {
-  const [a,setA]=useState(true)
+  const auth=useSelector((state)=>state.login.auth);
   return (
     <>
     <NavBar/>
       <Routes>
       <Route exact path='/' element={<Home></Home>}></Route>
       <Route path='/about' element={<About></About>}></Route>
+      <Route path='/login' element={<Login></Login>}></Route>
+      <Route path='/register' element={<Register></Register>}></Route>
       </Routes>
     </>
   )
