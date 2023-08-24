0025bed6b2eea573ad8ef85f9da1d50d546f66ec
ronaldgallardoarce <ronaldgallardoarce78@gmail.com>
2023-08-24 08:38:32 -0400

    copy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,9 @@ import { Route, Routes, useParams } from 'react-router-dom';
 import Home from './components/home/home';
 import NavBar from './components/navBar/navBar';
 import About from './components/about/about';
+import DemoApp from './components/Calendar/Calendar';
 import { useState } from 'react';
+import Calendario from './components/calendar/calendario';
 function App() {
   const [a,setA]=useState(true)
   return (
@@ -12,6 +14,8 @@ function App() {
       <Routes>
       <Route exact path='/' element={<Home></Home>}></Route>
       <Route path='/about' element={<About></About>}></Route>
+      <Route path='/calendar' element={<DemoApp></DemoApp>}></Route>
+      <Route path='/calendario' element={<Calendario></Calendario>}></Route>
       </Routes>
     </>
   )
