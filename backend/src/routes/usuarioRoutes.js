dc51fb40e3742828a2588b603e2ebab17358b4eb
olivio-git <subelzaolivitocabezas@gmail.com>
2023-08-24 08:34:56 -0400

    install M-UI

--- backend/src/routes/usuarioRoutes.js
+++ backend/src/routes/usuarioRoutes.js
@@ -1,11 +1,11 @@
 const express=require("express");
-const {getAllUsuarios,postUser, deleteById, updateById,validateUser}=require("../handlers/usuarioHandler");
+const {getAllUsuarios,postUser, deleteById, updateById,authLogin}=require("../handlers/usuarioHandler");
 const router=express();
 
 router.get('/',getAllUsuarios); //http://localhost:3001/api/users/   GET
 router.post('/',postUser); //http://localhost:3001/api/users/  POST
 router.delete('/delete/:id',deleteById);
 router.put('/update/:id',updateById);
-router.post('/access',validateUser)
+router.post('/login',authLogin)
 
 module.exports=router;
--- backend/src/services/jwtservice.js
+++ backend/src/services/jwtservice.js
@@ -4,41 +4,32 @@ const {Usuario}=require("../db");
 const keymaster="token_jwt_login";
 
 module.exports={
-    generateToken:async(user)=>{
-        console.log(user+"Usuario recibido")
-        try {
-            const payload={
-                userId:user.id_Usuario,
-                nombre:user.nombres,
-                ci:user.ci
+    signIn:async(user)=>{
+        return new Promise(async(resolve,reject)=>{
+            try {
+                const payload={
+                    _userId:user.id_Usuario,
+                    nombre:user.nombres,
+                    ci:user.ci
+                }
+                const options={
+                    expiresIn:'2h'
+                }
+                const tokengen=await jwt.sign(payload,keymaster,options);
+                resolve(tokengen);            
+            } catch (error) {
+                reject(error)
             }
-            const options={
-                expiresIn:'1h'
-            }
-            const tokengen=await jwt.sign(payload,keymaster,options);
-            console.log(tokengen);
-            return tokengen;
-        } catch (error) {
-            
-        }
+        })
     },
-    validateToken:async(token)=>{
+    requireToken:async(req,res,next)=>{
+        const user=await req.headers.authorization;
         try {
-            
-        } catch (error) {
-            
-        }
-    },
-    deleteToken:async(token)=>{
-
-    },
-    validateUser:async(req,res,next)=>{
-        try {
-            const user=req.body;
+            if(!user)res.status(401).json({messageError: 'Invalid authorization'});
+            req.user=user;
             next()
         } catch (error) {
-            res.status(401).json({error:error.message});
+            res.status(400).json({messageError:error.message});
         }
     }
-    //lucio sollit
 }
