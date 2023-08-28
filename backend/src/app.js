99830e8453bbc997e92b5e91dd6815ad8add08fe
olivio-git <subelzaolivitocabezas@gmail.com>
2023-08-28 09:09:09 -0400

    login and register

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,7 @@ server.use(cookieParser());
 server.use((req,res,next)=>{
     res.header("Access-Control-Allow-Origin","http://localhost:5173");
     res.header("Access-Control-Allow-Credentials","true");
-    res.header("Access-Control-Allow-Headers","Origin,X-Request-With, Content-Type,Accept");
+    res.header("Access-Control-Allow-Headers","Origin,X-Request-With, Content-Type,Accept,Authorization");
     res.header("Access-Control-Allow-Methods","GET,POST,PUT,DELETE");
     //res.header("Creator","");
     const now=new Date();  
