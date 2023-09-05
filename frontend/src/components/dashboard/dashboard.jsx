5a934668ee82946c61b5c165984ab6149a365ac4
limber-git <limber.rodrigo.tj@gmail.com>
2023-09-05 10:59:27 -0400

    GuardadoPreview

--- frontend/src/components/dashboard/dashboard.jsx
+++ frontend/src/components/dashboard/dashboard.jsx
@@ -2,23 +2,24 @@ import React from "react";
 import Sidebar from "./Sidebar";
 import ComponentForm from "./ComponentForm";
 import ComponentPreview from "./ComponentPreview";
-
 import "./Sidebar";
-import { useLocation } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import PublicationAdd from "./Publications/PublicationAdd";
+import PublicationPreview from "./Publications/PublicationPreview";
 
 function DashboardPage() {
-  const location = useLocation();
-  location.pathname
   return (
-    
+
     <div class="container">
       <div className="side-container">
         <Sidebar />
       </div>
       <div className="body-container">
-      
-        <ComponentForm />
-        <ComponentPreview />
+        <Routes>
+          <Route path="/dashboard/*" element={<PublicationAdd/>}></Route>
+          <Route path="/dashboard/publication/preview" element={<PublicationPreview/>}></Route>
+
+        </Routes>
       </div>
     </div>
   );
