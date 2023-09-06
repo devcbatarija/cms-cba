cee94e2d46c66b23beb0df2dd7c801f15d970d1e
olivio-git <subelzaolivitocabezas@gmail.com>
2023-09-06 10:40:06 -0400

    problems-container

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { Route, Routes, useLocation } from 'react-router-dom';
 import Home from './components/home/home';
 import NavBar from './components/navBar/navBar';
 import About from './components/about/about';
-import DemoApp from './components/Calendar/Calendar';
+import DemoApp from './components/calendar/calendar';
 import Calendario from './components/calendar/calendario';
 import Login from './components/auth/auth.login';
 import { useEffect } from 'react';
@@ -16,6 +16,7 @@ import Dashboard from './components/dashboard/dashboard';
 import NotFound from './components/Error/NotFound';
 
 import PublicationAdd from './components/dashboard/Publications/PublicationAdd.jsx';
+import TableUser from './components/dashboard/tableUser';
 
 function App() {
   const auth = useSelector((state) => state.login.auth);
@@ -62,11 +63,12 @@ function App() {
         {/* Ruta del dashboard, sin verificación de autenticación */}
         <Route path='/dashboard' element={<Dashboard></Dashboard>}>
           <Route path='/dashboard/publicaciones' element={<PublicationAdd />} />
-          
+          <Route path='/dashboard/tableuser' element={<TableUser></TableUser>}/>
         </Route>
         {/* Ruta para manejar páginas no encontradas */}
         <Route path='*' element={<NotFound />} />
       </Routes>
+
       {/* <Routes>
         <Route path='/dashboard' element={<PublicationAdd />} />
         </Routes> */}
