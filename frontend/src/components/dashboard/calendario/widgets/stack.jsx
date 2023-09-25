9fa63692238ad076c5ff65a937940b4746301184
ronaldgallardoarce <ronaldgallardoarce78@gmail.com>
2023-09-25 08:41:51 -0400

    Navbar actualizado

--- frontend/src/components/dashboard/calendario/widgets/stack.jsx
+++ frontend/src/components/dashboard/calendario/widgets/stack.jsx
@@ -4,7 +4,7 @@ import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import { styled } from '@mui/material/styles';
 import FiberManualRecordRoundedIcon from '@mui/icons-material/FiberManualRecordRounded';
-import ModalUpdateEvent from './modalUpdateEvent';
+import ModalUpdateEvent from '../modalUpdateEvent';
 import Icon from '@mui/material/Icon';
 
 const Item = styled(Paper)(({ theme }) => ({
