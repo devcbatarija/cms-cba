4ca46538a8d91d5a72bd0f24fcab9d7b162eafb9
limber-git <limber.rodrigo.tj@gmail.com>
2023-10-02 19:29:22 -0400

    EducationUSA

--- frontend/src/components/programs/adults.jsx
+++ frontend/src/components/programs/adults.jsx
@@ -8,7 +8,6 @@ const ProgramAdults = () => {
       <div className='w-full sm:w-9/10 md:w-8/10 lg:w-7/10 xl:w-6/10 2xl:w-5/10 mx-auto justify-center bg-gray-50'>
         <div className='InformationContainer'>
           <div id='1' className="program-container flex flex-col md:flex-row lg:flex-row items-center justify-center border-b border-gray-300">
-            {/* <div className="ImagenPrincipal w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 2xl:w-1/6 p-8"> */}
             <div className="ImagenPrincipal w-full md:w-1/2  p-0">
               <div style={{
                 backgroundImage: `url("https://costaaraucania.educacionpublica.cl/wp-content/uploads/2020/03/EDUMAT-2020-3.jpeg")`,
@@ -27,7 +26,6 @@ const ProgramAdults = () => {
               <br />
               <br />
               <div className='w-full sm:w-9/10 md:w-8/10 lg:w-7/10 xl:w-6/10 2xl:w-5/10 justify-center flex-col md:flex-row justify-center ' >
-                {/*w-full md:w-1/2 mt-4 p-5 rounded-lg text-center*/}
                 <p className="text-2xl font-bold text-black">¿QUÉ APRENDERÁ EN ESTE PROGRAMA?</p>
                 <br />
                 <div className='text-justify'>
@@ -42,7 +40,6 @@ const ProgramAdults = () => {
               </div>
             </div>
           </div>
-
           <div id='2' className="mt-4 p-4 rounded-lg text-gray-700 blur-10 flex flex-col md:flex-row border-b border-gray-300 ">
             <div className="w-full md:w-1/2 mt-4 p-2 rounded-lg text-center">
               <div className='imagen flex items-center '>
@@ -60,7 +57,6 @@ const ProgramAdults = () => {
                 </p>
               </div>
             </div>
-
             <div className="w-full md:w-1/2 mt-4 p-5 rounded-lg text-center">
               <div className='imagen flex items-center '>
                 <img className='imagenPequena w-15 h-20' src="https://th.bing.com/th/id/R.c162d9ccc85bd1bfa50bb9326ae63cbe?rik=d9UiAQcodmfAFA&riu=http%3a%2f%2fwww.galileo.edu%2fpage%2fwp-content%2fuploads%2f2020%2f10%2ficonos_requisitos.png&ehk=25%2fmHjHyN%2b6nwdBewY3XS5e4S4O57oYIPhyEbWcUItc%3d&risl=&pid=ImgRaw&r=0" alt="estudianteIcon" />
@@ -75,36 +71,34 @@ const ProgramAdults = () => {
               <br />
             </div>
           </div>
-
-
           <div className="padding-40  text-center">
             <div>
               <br />
-            <h2 className="text-2xl font-bold">PRECIOS DE NUESTROS CURSOS</h2>
-            <table className="border-collapse w-full sm:w-9/10 md:w-8/10 lg:w-7/10 xl:w-6/10 2xl:w-5/10 mx-auto text-center">
-              <thead>
-                <tr>
-                  <th className="py-2 px-4 border-b border-gray-300 text-black">Curso</th>
-                  <th className="py-2 px-4 border-b border-gray-300 text-black">Horario</th>
-                  <th className="py-2 px-4 border-b border-gray-300 text-black">Modalidad</th>
-                  <th className="py-2 px-4 border-b border-gray-300 text-black">Costo</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td className="py-2 px-4 border-b border-gray-300">Children</td>
-                  <td className="py-2 px-4 border-b border-gray-300">Tarde 16:15 17:45</td>
-                  <td className="py-2 px-4 border-b border-gray-300">Presencial</td>
-                  <td className="py-2 px-4 border-b border-gray-300">600</td>
-                </tr>
-                <tr>
-                  <td className="py-2 px-4 border-b border-gray-300">Children</td>
-                  <td className="py-2 px-4 border-b border-gray-300">Tarde 16:15 17:45</td>
-                  <td className="py-2 px-4 border-b border-gray-300">Presencial</td>
-                  <td className="py-2 px-4 border-b border-gray-300">600</td>
-                </tr>
-              </tbody>
-            </table>
+              <h2 className="text-2xl font-bold">PRECIOS DE NUESTROS CURSOS</h2>
+              <table className="border-collapse w-full sm:w-9/10 md:w-8/10 lg:w-7/10 xl:w-6/10 2xl:w-5/10 mx-auto text-center">
+                <thead>
+                  <tr>
+                    <th className="py-2 px-4 border-b border-gray-300 text-black">Curso</th>
+                    <th className="py-2 px-4 border-b border-gray-300 text-black">Horario</th>
+                    <th className="py-2 px-4 border-b border-gray-300 text-black">Modalidad</th>
+                    <th className="py-2 px-4 border-b border-gray-300 text-black">Costo</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td className="py-2 px-4 border-b border-gray-300">Children</td>
+                    <td className="py-2 px-4 border-b border-gray-300">Tarde 16:15 17:45</td>
+                    <td className="py-2 px-4 border-b border-gray-300">Presencial</td>
+                    <td className="py-2 px-4 border-b border-gray-300">600</td>
+                  </tr>
+                  <tr>
+                    <td className="py-2 px-4 border-b border-gray-300">Children</td>
+                    <td className="py-2 px-4 border-b border-gray-300">Tarde 16:15 17:45</td>
+                    <td className="py-2 px-4 border-b border-gray-300">Presencial</td>
+                    <td className="py-2 px-4 border-b border-gray-300">600</td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
@@ -114,7 +108,6 @@ const ProgramAdults = () => {
           <br />
         </div>
       </div>
-
     </>
   );
 }
