b3815ab021c0bee7a50a7e82e4938e31c0a19f60
ronaldgallardoarce <ronaldgallardoarce78@gmail.com>
2023-10-03 11:54:10 -0400

    guardar cambios

--- frontend/src/components/calendar/calendar.jsx
+++ frontend/src/components/calendar/calendar.jsx
@@ -8,9 +8,17 @@ import NavigateBeforeRoundedIcon from '@mui/icons-material/NavigateBeforeRounded
 import NavigateNextRoundedIcon from '@mui/icons-material/NavigateNextRounded';
 // import { Button } from '@mui/base/Button';
 import { styled } from "@mui/system";
-import {  useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import multimonth from "@fullcalendar/multimonth";
 import { Button } from "@mui/material";
+import Dropdown from '../dashboard/calendario/test';
+import dayjs from "dayjs";
+import 'dayjs/locale/es';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
+import axios from "axios";
+import EventList from './eventList';
+dayjs.extend(localizedFormat);
+dayjs.locale('es');
 
 const CalendarioClient = () => {
     const calendarRef = useRef(null);
@@ -27,18 +35,22 @@ const CalendarioClient = () => {
         let calendarApi = calendarRef.current.getApi();
         calendarApi.today();
     };
-    const changeView = (view) => {
-        let calendarApi = calendarRef.current.getApi();
-        calendarApi.changeView(view);
-    };
     const [title, setTitle] = useState('');
+    const [titleTable, setTitleTable] = useState('');
 
     const updateTitle = (e) => {
         //   let calendarApi = calendarRef.current.getApi();
         setTitle(e.view.title);
+        const currentMonth = dayjs(e.view.currentStart).format('MMMM')
+        setTitleTable(currentMonth);
+        const response = axios.get(`datosevento/getEventsByMonth/${e.view.currentStart}`).then(res => {
+            setEventsByMonth(res.data.results.Eventos);
+        }).catch(error => {
+            console.log(error)
+        })
     }
 
-    const handleEventClick= (e) => {
+    const handleEventClick = (e) => {
         let calendarApi = calendarRef.current.getApi();
         console.log(calendarApi)
         console.log(e);
@@ -46,26 +58,30 @@ const CalendarioClient = () => {
 
 
     const events = useSelector((state) => state.events.events);
-    
+    const [eventsByMonth, setEventsByMonth] = useState([])
+
     return (
         <>
-            <div className={"grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 min-h-full gap-4"}>
+            <div className={"grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 min-h-full lg:gap-10 p-10"}>
                 <div className="calendar col-span-2">
                     {/* seo declarar para consultas slang */}
-                    <div className='flex flex-row relative text-lg sm:text-xl md:text-2xl lg:text-2xl  '>
-                        <Button 
-                        sx={{width: '5px',margin:0, height: 'fit-content', backgroundColor: 'red', padding: 0}}
-                        onClick={prev}
-                        ><NavigateBeforeRoundedIcon /></Button>
-                        <h1 >{title}</h1>
-                        <Button onClick={next}><NavigateNextRoundedIcon /></Button>
-                        <div className='absolute right-0'>
-                            <Button onClick={goToToday}>Today</Button>
-                            <select onChange={(e) => changeView(e.target.value)}>
-                                <option value="dayGridMonth">Month</option>
-                                <option value="timeGridWeek">Week</option>
-                                <option value="multiMonthYear">Year</option>
-                            </select>
+                    <div className='items-center mb-1 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 relative text-lg sm:text-xl md:text-2xl lg:text-2xl '>
+                        <div className='order-last flex justify-center sm:flex-row md:flex-row lg:flex-row xl:flex-row sm:order-none sm:justify-start'>
+                            <Button
+                                sx={{ minWidth: 'fit-content', padding: '0', borderRadius: '50%' }}
+                                onClick={prev}
+                            ><NavigateBeforeRoundedIcon /></Button>
+
+                            <h1 className='uppercase font-semibold' >{title}</h1>
+                            <Button onClick={next}
+                                sx={{ minWidth: 'fit-content', padding: '0', borderRadius: '50%' }}
+                            ><NavigateNextRoundedIcon /></Button>
+                        </div>
+                        <div className='flex justify-end  text-base'>
+                            <Button onClick={goToToday}>Hoy</Button>
+                            <Dropdown
+                                calendarRef={calendarRef}
+                            />
                         </div>
                     </div>
                     <FullCalendar
@@ -88,8 +104,11 @@ const CalendarioClient = () => {
                         eventClick={handleEventClick}
                     />
                 </div>
-                <div className=" mt-5 ">
-                    <button onClick={handleEventClick}>select</button>
+                <div className="">
+                    <EventList
+                        title={titleTable}
+                        eventsByMonth={eventsByMonth}
+                    ></EventList>
                 </div>
             </div>
         </>
