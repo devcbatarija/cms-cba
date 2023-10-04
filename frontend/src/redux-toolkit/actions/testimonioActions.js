9397a5f806a7e305fc6a779a09b5e2f90f34d274
olivio-git <subelzaolivitocabezas@gmail.com>
2023-10-03 22:49:30 -0400

    navbar icons

--- frontend/src/redux-toolkit/actions/testimonioActions.js
+++ frontend/src/redux-toolkit/actions/testimonioActions.js
@@ -4,7 +4,6 @@ import axios from "axios";
 export const getAllTestimonio = createAsyncThunk("/getAllTestimonio", async () => {
   try {
     const response=await axios.get('testimonios/');
-    console.log(response)
     return response.data
   } catch (error) {
     return error.message;
