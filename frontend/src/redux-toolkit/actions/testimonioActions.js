c6ed0539bd3c0e17367e52f015f55ed20090069a
limber-git <limber.rodrigo.tj@gmail.com>
2023-10-24 10:22:11 -0400

    TableTestimonios

--- frontend/src/redux-toolkit/actions/testimonioActions.js
+++ frontend/src/redux-toolkit/actions/testimonioActions.js
@@ -3,9 +3,28 @@ import axios from "axios";
 
 export const getAllTestimonio = createAsyncThunk("/getAllTestimonio", async () => {
   try {
-    const response=await axios.get('testimonios/');
+    const response = await axios.get('testimonios/');
     return response.data
   } catch (error) {
     return error.message;
   }
+});
+export const deleteStateAllTestimonio = createAsyncThunk("/deleteStateAllTestimonio", async () => {
+  try {
+    return [];
+  } catch (error) {
+    return error.message;
+  }
+});
+export const selectAllTestimonio = createAsyncThunk("/selectAllTestimonio", async (testimonioIDs) => {
+  return testimonioIDs;
+});
+export const deselectAllTestimonio = createAsyncThunk("/deselectAllTestimonio", async () => {
+  return [];
+});
+export const selectTestimonio = createAsyncThunk("/selectTestimonio", async (testimonioID) => {
+  return testimonioID;
+});
+export const deselectTestimonio = createAsyncThunk("/deselectTestimonio", async (testimonioID) => {
+  return testimonioID;
 });
