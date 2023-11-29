7ec0b93d57bbb78213bd9c23defc78363250074a
limber-git <limber.rodrigo.tj@gmail.com>
2023-11-29 09:56:10 -0400

    revision

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,8 +7,8 @@ import { Provider } from 'react-redux'
 import store from './redux-toolkit/store/store.js';
 import axios from 'axios';
 
-axios.defaults.baseURL="https://www.webpage.cba.org.bo/api/";
-// axios.defaults.baseURL="https://backendcba.onrender.com/";
+axios.defaults.baseURL="https://www.webpage.cba.org.bo/appi/";
+//axios.defaults.baseURL="http://localhost:3001/api/";
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}> 
         <BrowserRouter>
