fa151becf17f328d7e110627eec55b5b99bb57ac
limber-git <limber.rodrigo.tj@gmail.com>
2023-12-21 10:32:32 -0400

    Last1

--- frontend/src/components/dashboard/Becas/BecaAdd.jsx
+++ frontend/src/components/dashboard/Becas/BecaAdd.jsx
@@ -1,138 +1,168 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import styled from "styled-components";
+import Uploader from "../Publications/Uploader";
+import axios from "axios";
+import { Grid, MenuItem, Select, Button } from "@mui/material";
+import { useEffect } from "react";
 
-function BecaAdd({ onPostCreate }) {
-  const [postData, setPostData] = useState({
-    title: '',
-    description: '',
-    status: '',
-    type: '',
-    multimedia: [],
-  });
+const Container = styled.div`
+  margin: 0 auto;
+  padding: 10px;
+  background-color: white;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  height: 90vh;
+  width: 100%;
+  overflow-y: overlay;
+  overflow-x: hidden;
+`;
+const Title = styled.h2`
+  color: #343a40;
+  font-Size:20px;
+`;
+
+const FormGroup = styled.div`
+  margin-bottom: 15px;
+`;
+
+const Label = styled.label`
+  display: block;
+  margin-bottom: 5px;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid #ced4da;
+`;
+
+const TextArea = styled.textarea`
+  width: 100%;
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid #ced4da;
+  min-height: 100px;
+  overflow-y: hidden;
+`;
+
+function BecaAdd({
+  beca,
+  setBeca,
+  handleSubmitBeca,
+}) {
+  const [urls, setUrls] = useState([]);
+  const [textAreaHeight, setTextAreaHeight] = useState("100px"); // Estado para controlar la altura del TextArea
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    setPostData({
-      ...postData,
-      [name]: value,
-    });
-  };
+    const property = e.target.name;
+    const value = e.target.value;
 
-  const handleMultimediaChange = (e) => {
-    const files = e.target.files;
-    setPostData({
-      ...postData,
-      multimedia: [...postData.multimedia, ...files],
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onPostCreate(postData);
-    setPostData({
-      title: '',
-      description: '',
-      status: '',
-      type: '',
-      multimedia: [],
-    });
-  };
-
-  const styles = {
-    container: {
-      margin: '0 auto',
-      width: '80%',
-      padding: '20px',
-      backgroundColor: '#f8f9fa',
-      borderRadius: '5px',
-      boxShadow: '0 0 10px rgba(0,0,0,0.1)'
-    },
-    title: {
-      color: '#343a40',
-      textAlign: 'center'
-    },
-    formGroup: {
-      marginBottom: '15px'
-    },
-    label: {
-      display: 'block',
-      marginBottom: '5px'
-    },
-    input: {
-      width: '100%',
-      padding: '10px',
-      borderRadius: '5px',
-      border: '1px solid #ced4da'
-    },
-    button: {
-      padding: '10px 20px',
-      backgroundColor: '#007bff',
-      color: '#fff',
-      border: 'none',
-      borderRadius: '5px',
-      cursor: 'pointer'
+    if (property != "multimedia") {
+      setBeca({
+        ...beca,
+        [property]: value,
+      });
+      return;
     }
   };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("/files/upload", {
+        filePath: beca.multimedia,
+        type: "image",
+      }).then(res=>{
+        handleSubmitBeca(res.data.results);
+      });
+    } catch (error) {
+      return error;
+    }
+  };
+  useEffect(() => {
+    // Calcula la altura del TextArea basándose en su contenido
+    setTextAreaHeight(`${beca.descripcion.split("\n").length * 25}px`);
+  }, [beca.descripcion]);
 
   return (
-    <div style={styles.container}>
-      <h2 style={styles.title}>Crear Publicación</h2>
-      <form onSubmit={handleSubmit}>
-        <div style={styles.formGroup}>
-          <label style={styles.label}>Título:</label>
-          <input
-            type="text"
-            name="title"
-            value={postData.title}
-            onChange={handleChange}
-            required
-            style={styles.input}
-          />
+    <>
+      <Container className="rounded-lg border rounded-lg">
+        <div className="flex flex-col items-center justify-center">
+          <Title>Crear beca</Title>
         </div>
-        <div style={styles.formGroup}>
-          <label style={styles.label}>Descripción:</label>
-          <textarea
-            name="description"
-            value={postData.description}
-            onChange={handleChange}
-            required
-            style={styles.input}
-          ></textarea>
-        </div>
-        <div style={styles.formGroup}>
-          <label style={styles.label}>Estado:</label>
-          <input
-            type="text"
-            name="status"
-            value={postData.status}
-            onChange={handleChange}
-            required
-            style={styles.input}
-          />
-        </div>
-        <div style={styles.formGroup}>
-          <label style={styles.label}>Tipo:</label>
-          <input
-            type="text"
-            name="type"
-            value={postData.type}
-            onChange={handleChange}
-            required
-            style={styles.input}
-          />
-        </div>
-        <div style={styles.formGroup}>
-          <label style={styles.label}>Subir Multimedia:</label>
-          <input
-            type="file"
-            accept="image/*, video/*"
-            multiple
-            onChange={handleMultimediaChange}
-            style={styles.input}
-          />
-        </div>
-        <button type="submit" style={styles.button}>Crear Publicación</button>
-      </form>
-    </div>
+        <form onSubmit={handleSubmit} className="flex flex-col rounded-lg ">
+          <FormGroup style={{ width: "100%" }}>
+            <Label>Título:</Label>
+            <Input
+              type="text"
+              name="titulo"
+              value={beca.titulo}
+              onChange={handleChange}
+              required
+            />
+          </FormGroup>
+          <FormGroup style={{ width: "100%" }}>
+            <Label>Descripción:</Label>
+            <TextArea
+              name="descripcion"
+              value={beca.descripcion}
+              onChange={handleChange}
+              style={{ height: textAreaHeight }}
+              required
+            ></TextArea>
+          </FormGroup>
+          <FormGroup
+            style={{ display: "flex", flexDirection: "row", gap: "20px" }}
+          >
+            <div>
+              <Label>Estado</Label>
+              <Select
+                labelId="demo-select-small-label"
+                id="estado"
+                value={beca.estado}
+                label="rol"
+                onChange={handleChange}
+                name="estado"
+              >
+                <MenuItem value="true">Visible</MenuItem>
+                <MenuItem value="false">Oculto</MenuItem>
+              </Select>
+            </div>
+            <div>
+              <Label>Tipo</Label>
+              <Select
+                labelId="demo-select-small-label"
+                id="tipo"
+                value={beca.tipo}
+                label="tipo"
+                onChange={handleChange}
+                name="tipo"
+              >
+                <MenuItem value="General">General</MenuItem>
+                <MenuItem value="Academico">Academico</MenuItem>
+              </Select>
+            </div>
+          </FormGroup>
+        </form>
+        <FormGroup>
+          <Label>Arrastre y suelte las imagenes:</Label>
+          <Uploader
+            urls={urls}
+            setUrls={setUrls}
+            publicacion={beca}
+            setPublicacion={setBeca}
+          ></Uploader>
+        </FormGroup>
+        <Grid sx={{ m: 1, width: "100%" }}>
+          <Button
+            variant="contained"
+            sx={{ width: "100%", borderRadius: "0px" }}
+            onClick={handleSubmit}
+          >
+            Publicar
+          </Button>
+        </Grid>
+      </Container>
+    </>
   );
 }
 
