9285517afadf1b9ac4b210efc6969301d4f2a4d2
ronaldgallardoarce <ronaldgallardoarce78@gmail.com>
2024-04-24 09:11:29 -0400

    new styles

--- frontend/src/components/navBar/positionedMenu.jsx
+++ frontend/src/components/navBar/positionedMenu.jsx
@@ -1,34 +1,47 @@
 import { Avatar } from "@mui/material";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { logoutSession } from "../../redux-toolkit/actions/auth.Actions";
 import Cookie from "js-cookie";
 import toast from "react-hot-toast";
 import { SuccessAlert } from "../toastAlerts/success";
 import { Profile } from "./profileUser";
 import axios from 'axios'
+import DashboardRoundedIcon from '@mui/icons-material/DashboardRounded';
+import LaptopChromebookRoundedIcon from '@mui/icons-material/LaptopChromebookRounded';
+import HelpOutlineRoundedIcon from '@mui/icons-material/HelpOutlineRounded';
+import PowerSettingsNewRoundedIcon from '@mui/icons-material/PowerSettingsNewRounded';
 
 const PositionedMenu = ({ altImg, srcImg, styles, nombres, apellidos }) => {
-  const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl);
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const divRef = useRef(null);
+
+  const handleClickOutside = (event) => {
+    if (divRef.current && !divRef.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
+  useEffect(() => {
+
+    document.addEventListener('click', handleClickOutside);
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
   const dispatch = useDispatch(false);
   const navigate = useNavigate();
   const rolUSer = useSelector((state) => state.login.user);
   const authlogin = useSelector((state) => state.login);
   const [openProfile, setOpenProfile] = useState(false);
-
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
   const handleLogout = async () => {
     try {
-      handleClose();
       navigate("/");
       const response = await axios.get('/users/logout')
       console.log(response);
@@ -53,72 +66,127 @@ const PositionedMenu = ({ altImg, srcImg, styles, nombres, apellidos }) => {
   useEffect(() => {
     aleatorios();
   }, [])
-  return (
-    <div>
-      {authlogin && !authlogin.user._profileImage ? (
-        <div
-          className="border p-2 rounded-[50%] 
-          w-[50px] h-[50px] text-white font-bold uppercase flex justify-center items-center"
-          style={{
-            background: localStorage.getItem("color"),
-          }}
-          onClick={handleClick}
-        >
-          {authlogin.user.nombres[0]}
-          {authlogin.user.apellidos[0]}
-        </div>
-      ) : (
-        <Avatar
-          id="demo-positioned-button"
-          aria-controls={open ? "demo-positioned-menu" : undefined}
-          aria-haspopup="true"
-          aria-expanded={open ? "true" : undefined}
-          onClick={handleClick}
-          alt={authlogin.user.correo}
-          src={authlogin.user._profileImage}
-          style={styles}
-        />
-      )}
 
-      <Menu
-        id="demo-positioned-menu"
-        aria-labelledby="demo-positioned-button"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        sx={{
-          top: "35px",
-          left: "10px",
-        }}
-        anchorOrigin={{
-          vertical: "top",
-          horizontal: "left",
-        }}
-        transformOrigin={{
-          vertical: "top",
-          horizontal: "left",
-        }}
-      >
-        <MenuItem onClick={() => setOpenProfile(true)}>Perfil</MenuItem>
-        {rolUSer.rol && rolUSer.rol == "Admin" ? (
-          <MenuItem component={Link} to="/" onClick={handleClose}>
-            Vista Cliente
-          </MenuItem>
-        ) : null}
-        {rolUSer.rol && rolUSer.rol == "Admin" ? (
-          <MenuItem component={Link} to="/dashboard" onClick={handleClose}>
-            Vista Administrador
-          </MenuItem>
-        ) : null}
-        <MenuItem onClick={handleLogout}>Cerrar sesion</MenuItem>
-        {openProfile ? (
-          <Profile
-            setOpenProfile={setOpenProfile}
-            userId={authlogin.user._userId}
-          ></Profile>
-        ) : null}
-      </Menu>
-    </div>
+  useEffect(() => {
+    if (openProfile) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  }, [openProfile]);
+
+  return (
+    <>
+      <div className="relative inline-block" ref={divRef} >
+        {/* Dropdown toggle button */}
+
+        {authlogin && !authlogin.user._profileImage ? (
+          <div
+            className="border p-2 rounded-[50%] 
+          w-[50px] h-[50px] text-white font-bold uppercase flex justify-center items-center"
+            style={{
+              background: localStorage.getItem("color"),
+            }}
+            onClick={toggleDropdown}
+          >
+            {authlogin.user.nombres[0]}
+            {authlogin.user.apellidos[0]}
+          </div>
+        ) : (
+          <Avatar
+            id="demo-positioned-button"
+            aria-controls={isOpen ? "demo-positioned-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={isOpen ? "true" : undefined}
+            onClick={toggleDropdown}
+            alt={authlogin.user.correo}
+            src={authlogin.user._profileImage}
+            style={styles}
+          />
+        )}
+        {/* Dropdown menu */}
+        {isOpen && (
+          <div
+            className="absolute right-0 z-20 w-60 py-2 mt-2 overflow-hidden origin-top-right bg-white rounded-md shadow-xl dark:bg-gray-800"
+          >
+            <div class="flex items-center p-3 -mt-2 text-sm text-gray-600 transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white">
+
+              {authlogin && !authlogin.user._profileImage ? (
+                <div
+                  className="border p-2 rounded-full
+                  w-9 h-9 text-white font-bold uppercase flex justify-center items-center mx-1 flex-shrink-0"
+                  style={{
+                    background: localStorage.getItem("color"),
+                  }}
+                >
+                  {authlogin.user.nombres[0]}
+                  {authlogin.user.apellidos[0]}
+                </div>
+              ) : (
+                <img class="flex-shrink-0 object-cover mx-1 rounded-full w-9 h-9" src={authlogin.user._profileImage} alt={authlogin.user.correo} />
+              )}
+
+              <div class="mx-1">
+                <h1 class="text-sm font-semibold text-gray-700 dark:text-gray-200 text-start">{authlogin.user.nombres} {authlogin.user.apellidos}</h1>
+                <p class="text-sm text-gray-500 dark:text-gray-400 break-all">{authlogin.user.correo}</p>
+              </div>
+            </div>
+            <hr className="border-gray-200 dark:border-gray-700" />
+
+            <div onClick={() => {
+              setOpenProfile(true)
+              toggleDropdown()
+            }} class="m-1.5 rounded-md flex items-center py-2 px-4 text-sm text-gray-600  transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
+              </svg>
+              <span class="mx-1 ml-5">
+                Ver perfil
+              </span>
+            </div>
+
+            {/* Opciones para el administrador */}
+            {rolUSer.rol && rolUSer.rol == "Admin" ? (
+              <>
+                <div onClick={() => navigate("/dashboard")} class="m-1.5 rounded-md flex items-center py-2 px-4 text-sm text-gray-600  transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white">
+                  <DashboardRoundedIcon />
+                  <span class="mx-1 ml-5">
+                    Dashboard
+                  </span>
+                </div>
+                <div onClick={() => navigate("/")} class="m-1.5 rounded-md flex items-center py-2 px-4 text-sm text-gray-600  transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white">
+                  <LaptopChromebookRoundedIcon />
+                  <span class="mx-1 ml-5">
+                    Sitio web
+                  </span>
+                </div>
+              </>
+            ) : null}
+            <hr className="border-gray-200 dark:border-gray-700" />
+
+            <div class="m-1.5 rounded-md flex items-center py-2 px-4 text-sm text-gray-600  transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white">
+              <HelpOutlineRoundedIcon />
+              <span class="mx-1 ml-5">
+                Ayuda
+              </span>
+            </div>
+            <div onClick={handleLogout} class="m-1.5 rounded-md flex items-center py-2 px-4 text-sm text-gray-600  transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white">
+              <PowerSettingsNewRoundedIcon />
+              <span class="mx-1 ml-5">
+                Cerrar sesion
+              </span>
+            </div>
+          </div>
+        )}
+      </div>
+      {openProfile ? (
+        <Profile
+          setOpenProfile={setOpenProfile}
+          userId={authlogin.user._userId}
+          openProfile={openProfile}
+        ></Profile>
+      ) : null}
+    </>
   );
 };
 export default PositionedMenu;  
