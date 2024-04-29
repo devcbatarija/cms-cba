b97dffbff57bc06138f7afaa2d8a5847537d5b71
ronaldgallardoarce <ronaldgallardoarce78@gmail.com>
2024-04-29 16:06:35 -0400

    new events table

--- backend/src/handlers/eventoHandler.js
+++ backend/src/handlers/eventoHandler.js
@@ -1,61 +1,71 @@
-const { addEvento, getAllEvento, updateEvento, getEventById, getActiveEvents } = require("../controllers/eventoController")
+const { addEvento, getAllEvento, updateEvento, getEventById, getActiveEvents, deleteEventoById } = require("../controllers/eventoController");
+const { response } = require("../utils");
 
 
-module.exports={
-    getAllEvento: async(req, res)=>{
+module.exports = {
+    getAllEvento: async (req, res) => {
         try {
-            const response=await getAllEvento();
+            const response = await getAllEvento();
             res.status(200).json({
-                results:response
+                results: response
             });
         } catch (error) {
-            res.status(404).json({error:error.message});
+            res.status(404).json({ error: error.message });
         }
     },
-    getActiveEvents: async(req, res)=>{
+    getActiveEvents: async (req, res) => {
         try {
-            const response=await getActiveEvents();
+            const response = await getActiveEvents();
             res.status(200).json({
-                results:response
+                results: response
             });
         } catch (error) {
-            res.status(404).json({error:error.message});
+            res.status(404).json({ error: error.message });
         }
     },
-    getEventById: async(req, res)=>{
+    getEventById: async (req, res) => {
         try {
-            const id=req.params.id;
-            const response=await getEventById(id);
+            const id = req.params.id;
+            const response = await getEventById(id);
             res.status(200).json({
-                results:response
+                results: response
             });
         } catch (error) {
-            res.status(404).json({error:error.message});
+            res.status(404).json({ error: error.message });
         }
     },
-    addEvento:async(req, res)=>{
+    deleteEventoById: async (req, res) => {
+        try {
+            const id = req.params.id;
+            const result = await deleteEventoById(id);
+            response(res, 200, { successMessage: "Evento eliminado exitosamente", result });
+        } catch (error) {
+            res.status(404).json({ error: error.message });
+        }
+    },
+    addEvento: async (req, res) => {
         try {
             const response = await addEvento(req.body);
             res.status(200).json({
-                results:response,
-                successMessage:"Se registro el evento correctamente"
+                results: response,
+                successMessage: "Se registro el evento correctamente"
             });
         } catch (error) {
-            res.status(400).json({error:error.message});
+            res.status(400).json({ error: error.message });
         }
     },
-    updateEvento:async(req, res)=>{
+    updateEvento: async (req, res) => {
         try {
-            const id=req.params.id;
-            const changes=req.body;
-            const response= await updateEvento(id, changes);
+            const id = req.params.id;
+            const changes = req.body;
+            const response = await updateEvento(id, changes);
             res.status(200).json({
-                results:response,
-                successMessage:"Se actualizaron los datos del evento correctamente"
+                results: response,
+                successMessage: "Se actualizaron los datos del evento correctamente"
             })
-            
+
         } catch (error) {
-            res.status(400).json({error:error.message});
+            res.status(400).json({ error: error.message });
         }
     }
 }
