8fdadb923f0987202fb2c2c768aea0499b1c680e
ronaldgallardoarce <ronaldgallardoarce78@gmail.com>
2024-04-30 16:13:18 -0400

    changes login

--- backend/src/services/jwtservice.js
+++ backend/src/services/jwtservice.js
@@ -1,8 +1,7 @@
 const jwt = require("jsonwebtoken");
 const { Usuario } = require("../db");
-const axios = require("axios"); 
-
-const keymaster = "token_jwt_login";
+const axios = require("axios");
+const { JWT_KEY_MASTER } = process.env
 
 module.exports = {
   signIn: async (user) => {
@@ -11,12 +10,13 @@ module.exports = {
         let payload = {
           _userId: user.id_Usuario,
           nombre: user.nombres,
-          ci: user.ci,
+          from: user.from,
+          accessToken: user.accessToken
         };
         if (user.from) {
           payload.from = user.from;
         }
-        const tokengen = await jwt.sign(payload, keymaster);
+        const tokengen = await jwt.sign(payload, JWT_KEY_MASTER);
         resolve(tokengen);
       } catch (error) {
         reject(error);
@@ -34,30 +34,32 @@ module.exports = {
       res.status(400).json({ messageError: error.message });
     }
   },
-  validToken: async (req, res) => {
+  validToken: async (req, res) => {        //para validar el inicio de sesion
     try {
       if (req.cookies.token) {
         let usResult;
         const token = req.cookies.token;
-        jwt.verify(token, keymaster, async (error, decoded) => {
+        jwt.verify(token, JWT_KEY_MASTER, async (error, decoded) => {
           const currentTime = Math.floor(Date.now() / 1000);
           if (decoded.from) {
             await axios
-              .post("http://localhost:8000/api/auth/userExists", {
+              .post("http://localhost:8000/api/auth/validate", {
                 id: decoded._userId,
+              }, {
+                headers: {
+                  'Authorization': `Bearer ${decoded.accessToken}`
+                }
               })
               .then((res) => {
-                if (res.data.exists) {
-                  usResult = {
-                    _userId: res.data.userData.id,
-                    _profileImage: res.data.userData.avatar,
-                    correo: res.data.userData.username,
-                    nombres: res.data.userData.fullName,
-                    apellidos: "",
-                    rol: res.data.userData.role,
-                    token,
-                  };
-                }
+                usResult = {
+                  _userId: res.data.userData.id,
+                  _profileImage: res.data.userData.avatar,
+                  correo: res.data.userData.username,
+                  nombres: res.data.userData.fullName,
+                  apellidos: "",
+                  rol: 'Client',
+                  token,
+                };
               });
           } else {
             const usLogin = await Usuario.findByPk(decoded._userId);
@@ -89,7 +91,7 @@ module.exports = {
       // if(!token){
       //   return res.status(200).json({ user: usResult });
       // }
-      jwt.verify(token, keymaster, async (error, decoded) => {
+      jwt.verify(token, JWT_KEY_MASTER, async (error, decoded) => {
         const currentTime = Math.floor(Date.now() / 1000);
         const usLogin = await Usuario.findByPk(decoded._userId);
         if (!usLogin) {
@@ -118,7 +120,7 @@ module.exports = {
         return res.status(401).json({ messageError: "Usuario no autorizado" });
       }
       const token = req.cookies.token;
-      jwt.verify(token, keymaster, async (error, decoded) => {
+      jwt.verify(token, JWT_KEY_MASTER, async (error, decoded) => {
         const usLogin = await Usuario.findByPk(decoded._userId);
         if (usLogin) {
           if (usLogin.rol == "Admin") {
@@ -141,13 +143,13 @@ module.exports = {
         res.status(401).json({ error: "Token no proporcionado" });
       }
       const tokenBearer = token.split(" ")[1];
-      jwt.verify(tokenBearer, keymaster, async (error, decoded) => {
+      jwt.verify(tokenBearer, JWT_KEY_MASTER, async (error, decoded) => {
         const usLogin = await Usuario.findByPk(decoded._userId);
         if (usLogin) {
           if (usLogin.rol == "Admin") {
             res.status(200).json({ MessageChannel: "User successfully valid" });
             return
-          }  
+          }
           res.status(401).json({ MessageChannel: "Usuario no autorizado" });
           return
         }
