07c2500cc102e7a2eb54e2abb9901b6f044368e5
olivio-git <subelzaolivitocabezas@gmail.com>
2024-05-03 17:53:58 -0400

    subida

--- backend/src/controllers/QRController.js
+++ backend/src/controllers/QRController.js
@@ -6,7 +6,7 @@ const { ClientError } = require("../utils/errors");
 const { response } = require("../utils");
 
 module.exports = {
-  generarQR: async (data) => { 
+  generarQR: async (data) => {
     const verificar = await QR.findOne({
       where: {
         id_Evento: data.id_Evento,
@@ -20,28 +20,58 @@ module.exports = {
     return verificar;
   },
   verificarQR: async ({ id_QR }) => {
-
-    const qr = await QR.findByPk(id_QR)
+    const qr = await QR.findByPk(id_QR);
     if (qr == null) {
       throw new ClientError("QR no encontrado");
     }
-    qr.update({ cantidad_uso: qr.cantidad_uso + 1 })
+    qr.update({ cantidad_uso: qr.cantidad_uso + 1 });
     return qr;
   },
   existsQR: async (data) => {
     const verificar = await QR.findOne({
       where: {
         id_Evento: data.id_Evento,
-        id_Estudiante: data.id_Estudiante
-      }
-    })
+        id_Estudiante: data.id_Estudiante,
+      },
+    });
     return verificar;
   },
-   // Función para reportes QR
-   reportEventQrParallels: async (data) => {
-    const eventos = find()
+  reportEventQrParallels: async (res, gestion, mes) => {
+    try {
+      const filteredQRs = await QR.findAll({
+        where: {
+          gestion: gestion,
+          mes_literal: mes,
+        },
+      });
+
+      if (filteredQRs.length === 0) {
+        throw new Error(
+          "No hay registros de QR para la gestión y mes especificados"
+        );
+      }
+      const groupedAndSummed = await filteredQRs.reduce((acc, qr) => {
+        const key = `${qr.paralelo}-${qr.gestion}-${qr.mes_literal}`;
+        
+        if (!acc[key]) {
+          acc[key] = {
+            paralelo: qr.paralelo,
+            cantidad_uso: 0,
+            profesor:qr.profesor
+          };
+        }
+        acc[key].cantidad_uso += qr.cantidad_uso;
+        return acc;
+      }, {});
+
+      // Convertir el resultado a un array para enviarlo como respuesta
+      const result = Object.values(groupedAndSummed);
+
+      response(res, 200, result);
+    } catch (error) {
+      console.error(error);
+      response(res, 500, { error: "Error interno del servidor" });
+    }
   },
-  reportStudentsQr:async ()=> {
-    
-  }
+  reportStudentsQr: async () => {},
 };
