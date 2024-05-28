b6d6f9f675818c711077b039006b1f703f94464f
limber-git <limber.rodrigo.tj@gmail.com>
2024-05-28 12:12:34 -0400

    Restored

--- frontend/src/components/publications/publications.jsx
+++ frontend/src/components/publications/publications.jsx
@@ -51,13 +51,7 @@ export const Publications = () => {
                                 <ImagenesEstilizadas multimedia={publicacion.multimedia} />
                             </div>
                             <dl className="pt-6 sm:pt-0 text-xs font-medium flex items-center sm:mt-1 row-start-3 md:mt-2.5 lg:row-start-2">
-                                {/* <dt className="sr-only">Reviews</dt>
-                            <dd className="text-indigo-600 flex items-center dark:text-indigo-400">
-                                <svg width="24" height="24" fill="none" aria-hidden="true" className="mr-1 stroke-current dark:stroke-indigo-500">
-                                    <path d="m12 5 2 5h5l-4 4 2.103 5L12 16l-5.103 3L9 14l-4-4h5l2-5Z" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                                </svg>
-                                <span>4.89 <span className="text-slate-400 font-normal">(128)</span></span>
-                            </dd> */}
+
                                 <dt><Avatar src="https://www.pagoexpress.com.bo/sitioweb/assets/images/cba.png" /></dt>
                                 <dd><span>Centro Boliviano Americano </span></dd>
                                 <dt className="sr-only">Location</dt>
